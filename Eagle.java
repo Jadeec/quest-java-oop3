1c58399df432c58cc4b2ae65913502b5a9076aa0
Jade <jade.cmail@gmail.com>
2021-12-03 16:04:09 +0100

    finished the interface quest

--- Eagle.java
+++ Eagle.java
@@ -1,24 +1,70 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
-    private boolean flying;
-    private int altitude;
+	// attributes
+	private boolean flying;
+	private int altitude;
+	private boolean landed;
 
-    public Eagle(String name) {
-        super(name);
-        this.flying = false;
-        this.altitude = 0;
-    }
+	// constructor
+	public Eagle(String name) {
+		super(name);
+		this.flying = false;
+		this.altitude = 0;
+		this.landed = false;
+	}
 
-    public int getAltitude() {
-        return altitude;
-    }
+	// getter/setter
+	public int getAltitude() {
+		return altitude;
+	}
 
-    public boolean isFlying() {
-        return flying;
-    }
+	public boolean isFlying() {
+		return flying;
+	}
 
-    @Override
-    public String sing() {
-        return "Screech!";
-    }
+	public boolean isLanded() {
+		return landed;
+	}
+
+	public void setLandedTrue() {
+		this.landed = true;
+	}
+
+	public void setLandedFalse() {
+		this.landed = false;
+	}
+
+	// Static methods form Fly interface
+	@Override
+	public String sing() {
+		return "Screech!";
+	}
+
+	@Override
+	public void takeOff() {
+		System.out.println("I'm taking off !");
+	}
+
+	@Override
+	public int ascend(int meters) {
+		System.out.println(" Going up " + meters + " meters");
+		return meters;
+	}
+
+	public int descend(int meters) {
+		System.out.println("Going down" + meters + " meters");
+		return meters;
+	}
+
+	@Override
+	public void land() {
+		if (!this.landed) {
+			setLandedTrue();
+		} else {
+			System.out.println(" landed!");
+			setLandedFalse();
+		}
+
+	}
+
 }
---
+++ Fly.java
@@ -0,0 +1,12 @@
+
+public interface Fly {
+
+	public void takeOff(); 
+	public int ascend(int meters); 
+	public int descend (int meters);
+	public void land(); 
+	 default public void glide() {
+	System.out.println("glides into the air.");
+	}
+	
+}
